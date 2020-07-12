c09d9d9fb44f489bf139a5786c9f12cac4e198ee
Justyna Wk <owcolot@gmail.com>
2020-07-12 17:07:37 +0200

    update CVformularz

--- src/test/java/selenium/automated/tests/CVformularzTests.java
+++ src/test/java/selenium/automated/tests/CVformularzTests.java
@@ -10,8 +10,11 @@ package selenium.automated.tests;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -23,7 +26,7 @@ public class CVformularzTests {
     @Before
     public void SetUp() {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Desktop\\SDA dokumenty\\SeleniumDrivers\\chromedriver.exe"); //ścieżka do chrome drivera
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Owca\\Desktop\\intelliJ - projekty\\SeleniumWebdrivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(URL);
@@ -34,6 +37,32 @@ public class CVformularzTests {
     @Test
     public void CVcreator() {
 
+        WebElement Szablon = driver.findElement(By.xpath("/html/body/main/section/div/section[2]/div/a[1]/img"));
+        Szablon.click();
+
+        WebElement RozpocznijButton = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div[2]/div[2]/a"));
+        RozpocznijButton.click();
+
+        WebElement firstName = driver.findElement(By.name("firstName"));
+        firstName.sendKeys("Figa");
+
+        WebElement lastName = driver.findElement(By.name("lastName"));
+        lastName.sendKeys("The Cat");
+
+        WebElement zawod = driver.findElement(By.className("UEZP0"));
+        zawod.sendKeys("Prezes Zarządu");
+
+        WebElement email = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[1]/div[2]/div[1]/div[2]/div/div[1]/div/div/div[3]/div[1]/div/div[1]/input"));
+        email.sendKeys("hiten.style@gmail.com");
+
+        WebElement telefon = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[1]/div[2]/div[1]/div[2]/div/div[1]/div/div/div[3]/div[2]/div/div[1]/input"));
+        telefon.sendKeys("123456789");
+
+        WebElement adres = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[1]/div[2]/div[1]/div[2]/div/div[1]/div/div/div[3]/div[3]/div/div[1]/textarea"));
+        adres.sendKeys("Urocza Gdynia\nDzielnica ze sporą ilością zieleni\nChata wypasiona nr 1");
+
+        //Select zawodWybor = new Select(driver.findElement(By.className("_2lnMd")));
+        //zawodWybor.selectByVisibleText("Prezes zarządu");
 
         }
 
